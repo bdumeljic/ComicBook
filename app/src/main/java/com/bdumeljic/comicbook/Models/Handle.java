bba1c2bdf26c92cd7b9981a819fe3e6e7439abb3
AileenPohl <aileenn.pohl@gmail.com>
2014-12-11 13:33:07 +0100

    No drag and drop as threading issues

---
+++ app/src/main/java/com/bdumeljic/comicbook/Models/Handle.java
@@ -0,0 +1,57 @@
+package com.bdumeljic.comicbook.Models;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+
+public class Handle {
+
+    Bitmap bitmap;
+    Context mContext;
+    Point point;
+    int id;
+
+    public Handle(Context context, Bitmap bitmap, Point point, int id) {
+        this.id = id;
+        this.bitmap = bitmap;
+        mContext = context;
+        this.point = point;
+    }
+
+    public int getWidthOfBall() {
+        return bitmap.getWidth();
+    }
+
+    public int getHeightOfBall() {
+        return bitmap.getHeight();
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public int getX() {
+        return point.x;
+    }
+
+    public int getY() {
+        return point.y;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public void setX(int x) {
+        point.x = x;
+    }
+
+    public void setY(int y) {
+        point.y = y;
+    }
+
+    public void setToCorner(int x, int y){
+        point.x = x;
+        point.y = y;
+    }
+}
